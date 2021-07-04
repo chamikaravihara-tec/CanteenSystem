5b7ceaa78938f3e5e9e05f4062ab93d2e6e0c91a
ravindutec123 <61771356+ravindutec123@users.noreply.github.com>
2021-07-04 12:16:08 +0530

    main page

--- demo/src/main/java/CanteenSystem/HomeController.java
+++ demo/src/main/java/CanteenSystem/HomeController.java
@@ -8,12 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-   // @RequestMapping(path = "/",method = RequestMethod.GET)
-   // public String getIndex(Model model)
-  //  {
-      //  return "canteensystemWeb/index";
-   // }
-
     @RequestMapping("/Login" )
     public String getLogin(Model model)
     {
@@ -23,6 +17,21 @@ public class HomeController {
     public String getIndex(Model model)
     {
         return "canteensystemWeb/index";
+    }
+
+    @RequestMapping("/User_Create")
+    public String getCreateUser(Model model){return "canteensystemWeb/forms/User_Create";}
+
+    @RequestMapping("/Menu")
+        public String getMenu(Model model)
+    {
+        return "canteensystemWeb/menu";
+    }
+
+    @RequestMapping("/Contact")
+    public String getOrder(Model model)
+    {
+        return "canteensystemWeb/contact";
     }
 
 
